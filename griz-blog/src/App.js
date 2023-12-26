dcac0e9a5c2a9f4165975b5be0b50368bb59b878
Evan C <ewchris101@gmail.com>
2023-12-26 17:06:31 -0500

    Tut5 - Multiple Components

--- griz-blog/src/App.js
+++ griz-blog/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Home from './Home';
+import NavBar from './NavBar';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const person = { name: 'Yoshi', age: 30 }
+
   return (
     <div className="App">
+      <NavBar />
       <div className="content">
-        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   );
---
+++ griz-blog/src/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <nav className="navBar">
+      <h1>Griz Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
