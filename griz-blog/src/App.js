6c6a060adc7afa4b8600beda019960cd032f1337
Evan C <ewchris101@gmail.com>
2023-12-26 17:31:16 -0500

    Tut6 - Adding Styles

--- griz-blog/src/App.js
+++ griz-blog/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Home from './Home';
 import NavBar from './NavBar';
 
--- griz-blog/src/NavBar.js
+++ griz-blog/src/NavBar.js
@@ -1,10 +1,18 @@
 const NavBar = () => {
+  const createButtonStyles = {
+    color: 'white',
+    backgroundColor: '#f1356d',
+    borderRadius: '8px'
+  }
+
   return (
     <nav className="navBar">
       <h1>Griz Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        <a href="/create" style={createButtonStyles}>
+          New Blog
+        </a>
       </div>
     </nav>
   );
