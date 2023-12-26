7f36583c83778ab0e35dd3919681b301e52aff3c
Evan C <ewchris101@gmail.com>
2023-12-26 17:42:33 -0500

    Tut7 - Click Events

--- griz-blog/src/Home.js
+++ griz-blog/src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log('Hello World!', e);
+  }
+
+  const handleClickToo = (name, e) => {
+    console.log('Hello ' + name + '!', e.target);
+  }
+
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={(e) => handleClickToo('Yoshi', e)}>Click Me Too!</button>
     </div>
   );
 }
