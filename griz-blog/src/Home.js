2557a8a2a17f0976192d663807afe112075099ce
Evan C <ewchris101@gmail.com>
2024-02-06 20:30:06 -0500

    Tut14 - event props

--- griz-blog/src/Home.js
+++ griz-blog/src/Home.js
@@ -1,23 +1,22 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-
   const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'Griz', id: 1 },
-    { title: 'Welcome Party!', body: 'lorem ipsum...', author: 'Griz', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Griz', id: 3 },
+    { title: 'My new website', body: 'lorem ipsum...', author: 'Mario', id: 1 },
+    { title: 'Welcome Party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario', id: 3 },
   ]);
+
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(b => b.id !== id);
+    setBlogs(newBlogs);
+  }
 
   return (
     <div className="home">
-      <h2>Home Page</h2>
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by: {blog.author}</p>
-          <p>{blog.body}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title={'Blogs'} handleDelete={handleDelete} />
     </div>
   );
 }
